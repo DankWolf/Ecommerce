80999d62aeda1eae9e5644e72127ab0a61eb6c9c
Christopher <chrisatenorio@gmail.com>
2023-07-20 21:52:54 -0700

    category complete

---
+++ src/data.js
@@ -0,0 +1,41 @@
+export const sliderItems = [
+  {
+    id: 1,
+    title: "SUMMER SALE",
+    img: "https://images.unsplash.com/photo-1509868918748-a554ad25f858?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "#f5fafd",
+  },
+  {
+    id: 2,
+    title: "WINTER SALE",
+    img: "https://images.unsplash.com/photo-1612096536102-93f503aa2419?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8d2ludGVyJTIwZmFzaGlvbnxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "#fcf1ed",
+  },
+  {
+    id: 3,
+    title: "GRAND SALE",
+    img: "https://images.unsplash.com/photo-1550614000-4895a10e1bfd?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mjh8fGZhc2hpb258ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "##fbf0f4",
+  },
+];
+
+export const categories = [
+  {
+    id: 1,
+    img: "https://images.unsplash.com/photo-1564430360553-441e3dbce543?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NTl8fGZhc2hpb24lMjBzaGlydHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60",
+    title: "SHIRT STYLE!",
+  },
+  {
+    id: 2,
+    img: "https://plus.unsplash.com/premium_photo-1683141468474-f76e8eb5a25c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OTF8fGZhc2hpb24lMjBsb3VuZ2V8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60",
+    title: "LOUNGEWEAR LOVE",
+  },
+  {
+    id: 3,
+    img: "https://images.unsplash.com/photo-1506424839817-5ec0530fc5e0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjM4fHxmYXNoaW9uJTIwamFja2V0fGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60",
+    title: "LIGHT JACKETS",
+  },
+];
